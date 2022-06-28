1b114a2dc5a46ffdb965feb8a439e6297ff85844
Rodrigo Pedroza Barcelos <rodrigobeavis@yahoo.com.br>
2022-06-28 20:15:41 -0300

    add inicial

---
+++ MyClasses/FileProcess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClasses
+{
+    class FileProcess
+    {
+
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("fileName");
+            }
+
+            return File.Exists(fileName);
+        }
+    }
+}
